0d1e409b6a04a654270144a842d215e607be780f
Ilya Belenkiy <ilya.belenkiy@gmail.com>
2023-03-15 21:43:34 -0400

    Add templates

---
+++ Sources/ReducerArchitecture/ReducerArchitectureTemplates.swift
@@ -0,0 +1,71 @@
+//
+//  ReducerArchitectureTemplates.swift
+//
+//  Created by Ilya Belenkiy on 3/15/23.
+//
+
+import Foundation
+
+/*
+// Reducer
+ 
+import ReducerArchitecture
+
+enum <#StoreNsp#>: StoreNamespace {
+    typealias PublishedValue = <#Value#>
+    
+    struct StoreEnvironment {
+    }
+    
+    enum MutatingAction {
+    }
+    
+    enum EffectAction {
+    }
+    
+    struct StoreState {
+    }
+}
+
+extension <#StoreNsp#> {
+    @MainActor
+    static func reducer() -> Reducer {
+        .init { state, action in
+            switch action {
+            }
+        }
+    }
+    
+    @MainActor
+    static func reducer() -> Reducer {
+        .init(
+            run: { state, action in
+                switch action {
+                }
+            },
+            effect: { env, state, action in
+                switch action {
+                }
+            }
+        )
+    }
+}
+*/
+
+/*
+// StoreUI
+
+import SwiftUI
+import ReducerArchitecture
+
+extension <#StoreNsp#>: StoreUIWrapper {
+    struct ContentView: StoreContentView {
+        typealias StoreWrapper = <#StoreNsp#>
+        @ObservedObject var store: Store
+        
+        var body: some View {
+            Text("hello")
+        }
+    }
+}
+*/
