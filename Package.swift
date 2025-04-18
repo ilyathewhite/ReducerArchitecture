3322dbdcd770dceb0af6a789c92f5fc5c0b9a1a6
Ilya Belenkiy <ilya.belenkiy@gmail.com>
2025-04-18 11:44:20 -0400

    Update dependency on CombineEx

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ilyathewhite/CombineEx.git", .branch("main")),
-        .package(url: "https://github.com/ilyathewhite/FoundationEx.git", .branch("main")),
+        .package(url: "https://github.com/ilyathewhite/CombineEx.git", .upToNextMinor(from: "1.0.3"))
     ],
     targets: [
         .target(
