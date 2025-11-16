10b8fd689ed9ed880abcfeb0086f117721b83ae4
Ilya Belenkiy <ilya.belenkiy@gmail.com>
2025-11-15 22:40:03 -0500

    Refactoring

--- Sources/ReducerArchitecture/ReducerArchitectureNavigation.swift
+++ Sources/ReducerArchitecture/ReducerArchitectureNavigation.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension UIKitNavigationFlow where Nsp: StoreUINamespace {
-    public init(_ root: Nsp.Store, _ run: @escaping (Nsp.PublishedValue, _: NavigationProxy) async -> Void) {
+    public init(_ root: Nsp.Store, _ run: @escaping (Nsp.PublishedValue, _ proxy: NavigationProxy) async -> Void) {
         self.init(root: root, run: run)
     }
 }
@@ -20,7 +20,7 @@ extension UIKitNavigationFlow where Nsp: StoreUINamespace {
 import SwiftUI
 
 extension NavigationFlow where Nsp: StoreUINamespace {
-    public init(_ root: Nsp.Store, _ run: @escaping (Nsp.PublishedValue, _: NavigationProxy) async -> Void) {
+    public init(_ root: Nsp.Store, _ run: @escaping (Nsp.PublishedValue, _ proxy: NavigationProxy) async -> Void) {
         self.init(root: root, run: run)
     }
 }
@@ -30,14 +30,14 @@ extension NavigationFlow where Nsp: StoreUINamespace {
 #if os(macOS)
 
 extension CustomNavigationFlow where Nsp: StoreUINamespace {
-    public init(_ root: Nsp.Store, _ run: @escaping (Nsp.PublishedValue, _: NavigationProxy) async -> Void) {
+    public init(_ root: Nsp.Store, _ run: @escaping (Nsp.PublishedValue, _ proxy: NavigationProxy) async -> Void) {
         self.init(root: root, run: run)
     }
 }
 
 #endif
 
-extension NavigationTestProxy {
+extension TestNavigationProxy {
     @MainActor
     public func getStore<Nsp: StoreUINamespace>(_ type: Nsp.Type, _ timeIndex: inout Int) async throws -> Nsp.Store {
         let value = await currentViewModelPublisher.values.first(where: { $0.timeIndex == timeIndex })
@@ -65,3 +65,4 @@ extension StoreUI {
         self.init(store)
     }
 }
+
